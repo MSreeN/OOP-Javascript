7d5aaf03a261a0fa327a63aeafda99aef1a06905
MSreeN <khyathimnsree369@gmail.com>
2023-05-12 18:54:11 +0530

    setting properties for object

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function Person(firstName, birthYear){
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  console.log(this);
+}
+
+const sree = new Person("sree", new Date(1,1,15))
+
+console.log(sree);
