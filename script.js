50da73bbc4004ca3518f8fc6ab3092d737ee2547
MSreeN <khyathimnsree369@gmail.com>
2023-05-12 19:32:44 +0530

    method inside constructor function

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 function Person(firstName, birthYear){
   this.firstName = firstName;
   this.birthYear = birthYear;
-  console.log(this);
+  this.calcAge = function(){
+     return `${this.firstName} age is ${new Date().getFullYear() - new Date(this.birthYear).getFullYear()}`
+  }
+  console.log(this.calcAge());
 }
 
-const sree = new Person("sree", new Date(1,1,15))
+const sree = new Person("sree", new Date("October 25 2001"))
 
-
+console.log(new Date());
 
 //creating objs using constructor function
 
 const ram = new Person("ram", "seth")
 const rakesh = new Person("rakesh")
+
+console.log(rakesh instanceof(Person));
