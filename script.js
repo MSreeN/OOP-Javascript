76e6bba62491c95935af213b1da666363055a3d4
MSreeN <khyathimnsree369@gmail.com>
2023-05-26 21:13:28 +0530

    isPrototypeOf()

--- script.js
+++ script.js
@@ -55,4 +55,7 @@ console.log(mahesh, "&" , mahesh.calcAge());
 console.log(naga);
 
 //Every object has a property prototype, when we print naga is the console we can see prototype object in naga but it doesn't mean that every object like naga is carrying that prototype property around, it is just the link to the prototype object.
-console.log(naga.__proto__);
+console.log(naga.__proto__ === Person.prototype);
+
+//Person has prototype object and all the objects created using person has link to it so that is why above and below lines are yielding true.
+console.log(Person.prototype.isPrototypeOf(naga));
