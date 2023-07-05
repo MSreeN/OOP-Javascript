bf8d3b710d32acaf282a88916b41229cfb73cdab
MSreeN <khyathimnsree369@gmail.com>
2023-07-05 22:30:11 +0530

    challenge 4

--- script.js
+++ script.js
@@ -533,3 +533,53 @@ console.log(sreeAcc);
 sreeAcc.deposit(500).withdraw(400).requestLoan(100).withdraw(200);
 
 console.log("after chaining methods", sreeAcc);
+
+
+// The Complete JavaScript Course 29
+// Coding Challenge #4
+// Your tasks:
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+// Test data:
+// § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+
+// class CarCl{
+//   constructor(make, speed){
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate(){
+//     this.speed = this.speed + 10;
+//     console.log(`current speed is ${this.speed}`);
+//   }
+
+//   brake(){
+//     this.speed -= 5;
+//     console.log(`current speed is ${this.speed}`);
+//   }
+
+//   get speedUs(){
+//     console.log(`current speed of car(${this.speed}) in miles/hour is ${this.speed/1.6}`);
+//   }
+
+//   set speedUs(speed){
+//     this.speed = speed * 1.6;
+//     console.log(`${speed} in kilometers/hour is ${this.speed}`);
+//   }
+// }
+
+
+class EVCL extends CarCl{
+  #charge;
+  constructor(make, speed ,charge){
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  
+}
